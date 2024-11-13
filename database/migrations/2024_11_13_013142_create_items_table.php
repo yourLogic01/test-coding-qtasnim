86ee2d81416da99e4e451b9a0fdd9f7a73341f70
yourLogic01 <2010631170004@student.unsika.ac.id>
2024-11-13 08:41:53 +0700

    feat(migration) : add migrations

---
+++ database/migrations/2024_11_13_013142_create_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->integer('stock');
+            $table->foreignId('type_of_item_id')->constrained('type_of_items','id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
